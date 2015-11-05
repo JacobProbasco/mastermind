d42d5e7ca72fe60a1d08de75ae396df0bd56cfa5
JacobProbasco <jacobprobasco@gmail.com>
2015-11-05 10:06:14 -0500

    Add initial Mastermind file

---
+++ main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  mastermind
+//
+//  Created by Jacob Probasco on 11/4/15.
+//  Copyright © 2015 jprobasco. All rights reserved.
+
+#include <stdio.h>
+/*
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+*/
+
+int main(void) {
+
+    
+
+    
+}
+
+
+
